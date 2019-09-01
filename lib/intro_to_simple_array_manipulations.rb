e336b46800beb0a4a92128c6015f2e003dac8d0f
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-01 18:17:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,10 +27,11 @@ def using_concat(arrayone, arraytwo)
 end 
 
 def using_insert(array, element)
-  
+  array.insert(4, element)
 end 
 
 def using_uniq(array)
+  
 end 
 
 def using_flatten(array)
